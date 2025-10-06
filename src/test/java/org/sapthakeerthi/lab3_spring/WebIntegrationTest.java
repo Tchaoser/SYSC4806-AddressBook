bcaa8e8927a7fd301a58a023ad4a68795af26c5a
Tchaoser <113814746+Tchaoser@users.noreply.github.com>
2025-10-05 22:03:42 -0400

    adding spring boot test for auto testing

---
+++ src/test/java/org/sapthakeerthi/lab3_spring/WebIntegrationTest.java
@@ -0,0 +1,13 @@
+package org.sapthakeerthi.lab3_spring;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Test;
+
+@SpringBootTest
+class WebIntegrationTest {
+
+    @Test
+    void contextLoads() {
+
+    }
+}
